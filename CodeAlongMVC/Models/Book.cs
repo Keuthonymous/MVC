679bb55ebaf3911eece6554a3d8d0209ecc61d22
Keuthonymous <mikie.daughtrey@gmail.com>
2017-05-22 11:08:16 +0200

    Added book class

--- CodeAlongMVC/Models/Book.cs
+++ CodeAlongMVC/Models/Book.cs
@@ -7,5 +7,10 @@ namespace CodeAlongMVC.Models
 {
     public class Book
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
     }
 }
